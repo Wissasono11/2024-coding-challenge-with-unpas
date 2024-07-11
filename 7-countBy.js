7fff219f8d32dabcba24e58e01b7fb51d47316ab
Wissasono11 <bayuwicaksono782@gmai.com>
2024-07-11 20:48:48 +0700

    Day 7/366

---
+++ 7-countBy.js
@@ -0,0 +1,23 @@
+//2024 Coding Challenge with WPU
+//day 6/366
+
+// function countBy(x, n) {
+//     let z = [];
+//     for(let i = 1; i<=n; i++){
+//         z.push(i * x);
+//     }
+//     return z;
+//   }
+//   // x=kelipatan bilangan
+//   // n=panjang arraynya
+// console.log(countBy(2,10));
+
+//another vers
+// function countBy(x, n) {
+//     return [...Array(n)].map((el, i) => (i + 1) * x);
+// }
+// console.log(countBy(3, 2));
+
+const countBy = (x, n) => [...Array(n)].map((_, i) => (i + 1) * x);
+
+console.log(countBy(2, 5))
