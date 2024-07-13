665d6d07261d638379429cd0b9b1387d1f8f4a36
Bayu Wicaksono <143940265+Wissasono11@users.noreply.github.com>
2024-07-13 23:24:53 +0700

    Update 9-maxmin.js

--- 9-maxmin.js
+++ 9-maxmin.js
@@ -4,7 +4,7 @@
 // var min = function(list){
 //     let minValue = list[0];
 //     for(let i = 1; i < list.length; i++){
-//         if (minValue > list[i]){
+//         if (minValue > list[i]){ 
 //             minValue = list[i]
 //         }
 //     }
