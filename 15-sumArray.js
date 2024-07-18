cbfad8eb3ef6d3566f97804e4305c3a6b08ca4b4
Wissasono11 <bayuwicaksono782@gmai.com>
2024-07-18 10:02:22 +0700

    day 15/366 update

--- 15-sumArray.js
+++ 15-sumArray.js
@@ -3,28 +3,28 @@
 //https://www.codewars.com/kata/576b93db1129fcf2200001e6
 
 // function sumArray(array) {
-//     if(array == null || array.length <= 2) return 0;
-//     const sorted = array.sort((a,b) => a-b) 
-//          // return sorted -> [1, 2, 6, 8, 10]
-//     let result = 0;
-//     for (i = 1; i < sorted.length - 1; i++){
-//         result += sorted[i]; 
-//     }
-//     return result;
+    if(array == null || array.length <= 2) return 0;
+    const sorted = array.sort((a,b) => a-b) 
+         // return sorted -> [1, 2, 6, 8, 10]
+    let result = 0;
+    for (i = 1; i < sorted.length - 1; i++){
+        result += sorted[i]; 
+    }
+    return result;
 
-// }
-// console.log(sumArray([ -6, -20, -1, -10, -12 ]));
+}
+console.log(sumArray([ -6, -20, -1, -10, -12 ]));
 
 //slice reduce vers
-// function sumArray(array) {
-//     if(array == null || array.length <= 2) return 0;
-//     return array.sort((a,b) => a-b).slice(1, -1).reduce((acc, curr) => acc + curr) 
-// }
-// console.log(sumArray([ 6, 2, 1, 8, 10 ]));
+function sumArray(array) {
+    if(array == null || array.length <= 2) return 0;
+    return array.sort((a,b) => a-b).slice(1, -1).reduce((acc, curr) => acc + curr) 
+}
+console.log(sumArray([ 6, 2, 1, 8, 10 ]));
 
 //one-liner lover
-// const sumArray = (array) => array == null || array.length <= 2 ? 0 : array.sort((a,b) => a-b).slice(1, -1).reduce((acc, curr) => acc + curr)
-// console.log(sumArray([ 6, 2, 1, 8, 10 ]));
+const sumArray = (array) => array == null || array.length <= 2 ? 0 : array.sort((a,b) => a-b).slice(1, -1).reduce((acc, curr) => acc + curr)
+console.log(sumArray([ 6, 2, 1, 8, 10 ]));
 
 const sumArray = array => array ? array.sort((a,b) => a-b).slice(1, -1).reduce((acc, curr) => acc + curr, 0) : 0
 console.log(sumArray([ 6, 2, 1, 8, 10 ]));
