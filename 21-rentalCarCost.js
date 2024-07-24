29a9fd2739dc2068ee53ca20c70ee8551f0c2803
Wissasono11 <bayuwicaksono782@gmai.com>
2024-07-24 20:42:16 +0700

    day 21/366

---
+++ 21-rentalCarCost.js
@@ -0,0 +1,23 @@
+//2024 Coding Challenge with WPU
+//day 20/366
+//https://www.codewars.com/kata/568d0dd208ee69389d000016
+
+function rentalCarCost(d) {
+    let discount = 0;
+    if(d >= 7) {
+        discount = 50;
+    } else if(d >= 3) {
+        discount = 20;
+    }
+    return (d * 40) - discount;
+}
+
+// ternary vers
+function rentalCarCost(d) {
+    return d * 40 - (d >= 7 ? 50 : d >= 3 ? 20: 0);
+}
+
+//one-liner vers
+const rentalCarCost = d => d * 40 - (d >= 7 ? 50 : d >= 3 ? 20: 0);
+
+console.log(rentalCarCost(7));
