c32896003e9d7fde2e9b9ce0c9c5b198cb72f57c
Wissasono11 <bayuwicaksono782@gmai.com>
2024-07-29 23:29:11 +0700

    day 38/366

---
+++ 38-strCount.js
@@ -0,0 +1,22 @@
+// WPU Coding Challenge 2024
+// 37/366
+// https://www.codewars.com/kata/5865918c6b569962950002a1
+
+function strCount(str, letter){
+    let countLetter = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === letter){
+           countLetter++ 
+        }
+    }
+    return countLetter
+}
+
+//split vers 
+function strCount(str, letter){
+    return str.split(letter).length - 1
+}
+
+//one-liner vers
+const strCount = (str, letter) => str.split(letter).length - 1
+console.log(strCount('Klauss', 's'))
