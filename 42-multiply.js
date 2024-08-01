8081260407ea8da5ef35a6b9f78c439662671b87
Wissasono11 <bayuwicaksono782@gmai.com>
2024-08-01 20:35:37 +0700

    day 42/366

---
+++ 42-multiply.js
@@ -0,0 +1,11 @@
+// 42/366
+//https://www.codewars.com/kata/50654ddff44f800200000004
+
+function multiply(a,b){
+    return a * b
+}
+
+//one-liner lover
+const multiply = (a, b) => a * b
+
+console.log(multiply(4, 4))
